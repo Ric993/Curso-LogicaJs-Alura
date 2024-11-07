11df0c6916bc332564a731e869a6abde4c093c34
Richard Ferreira <richard.ferreira.1@gmail.com>
2024-11-07 08:01:36 -0300

    ex02

---
+++ ex/ex02.js
@@ -0,0 +1,18 @@
+let dia = prompt("Que dia é hoje?")
+console.log(dia)
+
+if(dia == "sabado" || dia == "domingo") {
+    alert(`Bom fim de semana!`)
+} else {
+    alert(`Boa semana!`)
+}
+
+let num = prompt(`Digite um numero positivo ou negativo!`)
+console.log(num)
+
+if(num >= 0) {
+    alert(`Seu numero ${num} é positivo!`)
+} else {
+    alert(`Seu numero ${num} é negativo!`)
+}
+
--- logica-js-projeto_inicial/app.js
+++ logica-js-projeto_inicial/app.js
@@ -4,7 +4,7 @@ let chute = prompt(`Escolha um numero entre 1 e 30`);
 
 
 if (chute == numeroSecreto) {
-    alert(`Isso ai! Você descobriu o número secreto! (29)`);
+    alert(`Isso ai! Você descobriu o número secreto! ${numeroSecreto}`);
 
 } else {
     alert(` Você errou :( `)
