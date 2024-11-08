49ecbd9bd5b1d7d2a08df6189554974d93ffb637
Richard Ferreira <richard.ferreira.1@gmail.com>
2024-11-08 10:10:31 -0200

    ex e dica

---
+++ ex/ex03.js
@@ -0,0 +1,24 @@
+let contador = 1;
+while (contador <= 10) {
+  alert(contador);
+  contador++;
+}
+
+/*let contador = 10;
+while (contador >= 0) {
+  alert(contador);
+  contador--;
+}*/
+
+/*let numero = parseInt(prompt("Digite um número para a contagem regressiva:"));
+while (numero >= 0) {
+  alert(numero);
+  numero--;
+}*/
+
+/*let numero = parseInt(prompt("Digite um número para a contagem progressiva:"));
+let contador = 0;
+while (contador <= numero) {
+  console.log(contador);
+  contador++;
+}*/
