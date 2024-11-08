94c628d5f4bf6ba0cde3d5216c0ca7b03c5b4b94
Richard Ferreira <richard.ferreira.1@gmail.com>
2024-11-08 10:09:25 -0200

    while

--- logica-js-projeto_inicial/app.js
+++ logica-js-projeto_inicial/app.js
@@ -1,11 +1,23 @@
 alert(`Boas vindas ao jogo do número secreto`);
-let numeroSecreto = 29;
-let chute = prompt(`Escolha um numero entre 1 e 30`);
+let numeroSecreto = 15;
+console.log(numeroSecreto);
+let chute ;
+let tentativas = 1;
 
-
-if (chute == numeroSecreto) {
-    alert(`Isso ai! Você descobriu o número secreto! ${numeroSecreto}`);
-
-} else {
-    alert(` Você errou :( `)
+while (chute != numeroSecreto) {
+    chute = prompt(`Escolha um numero entre 1 e 30`)
+    
+    if (chute == numeroSecreto) {
+        alert(`Isso ai! Você descobriu o número secreto! ${numeroSecreto} com ${tentativas} tentativas!`);
+    
+    } else {
+        if (numeroSecreto > chute) {
+            alert(`O numero é maior que o ${chute}`);
+        } else {
+            alert(`O numero é menor que o ${chute}`);
+        }
+        //tentativas = tentativas + 1;
+        tentativas++;
+    }
 }
+
