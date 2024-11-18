c96b392a8875027bf94466d8194c267863f4242a
Richard Ferreira <richard.ferreira.1@gmail.com>
2024-11-18 08:29:16 -0300

    num aleatório inserido

---
+++ ex/ex04.js
@@ -0,0 +1,3 @@
+let numero = prompt(`Digite um numero`)
+refnumero = numero < 0? `Negativo` : `Positivo`;
+alert(`Seu numero ${numero} é ${refnumero}`)
--- logica-js-projeto_inicial/app.js
+++ logica-js-projeto_inicial/app.js
@@ -1,14 +1,15 @@
 alert(`Boas vindas ao jogo do número secreto`);
-let numeroSecreto = 15;
+let numeroMaximo = 1000;
+let numeroSecreto = parseInt(Math.random() * numeroMaximo + 1); //gera numero aleatorio
 console.log(numeroSecreto);
 let chute ;
 let tentativas = 1;
 
 while (chute != numeroSecreto) {
-    chute = prompt(`Escolha um numero entre 1 e 30`)
+    chute = prompt(`Escolha um numero entre 1 e ${numeroMaximo}`)
     
     if (chute == numeroSecreto) {
-        alert(`Isso ai! Você descobriu o número secreto! ${numeroSecreto} com ${tentativas} tentativas!`);
+        break;
     
     } else {
         if (numeroSecreto > chute) {
@@ -21,3 +22,11 @@ while (chute != numeroSecreto) {
     }
 }
 
+let palavraTentativa = tentativas > 1 ? `tentativas` : `tentativa`
+alert(`Isso ai! Você descobriu o número secreto! ${numeroSecreto} com ${tentativas} ${palavraTentativa}!`);
+
+/*if(tentativas > 1) {
+    alert(`Isso ai! Você descobriu o número secreto! ${numeroSecreto} com ${tentativas} tentativas!`);
+} else {
+    alert(`Isso ai! Você descobriu o número secreto! ${numeroSecreto} com ${tentativas} tentativa!`);
+}*/
